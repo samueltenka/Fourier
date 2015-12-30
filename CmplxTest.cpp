8c51840c0720818ff066184f5ef93509a38cf78a
samtenka <samtenka@umich.edu>
2015-12-30 01:27:38 -0500

    just a sanity check of Cmplx::mag2(), ::mag(). passed.

--- CmplxTest.cpp
+++ CmplxTest.cpp
@@ -10,16 +10,21 @@ int main(int argc, char** argv) {
    Cmplx b(0, 2.0);
 
    //1. Test print functions
+   std::cout << "Test print functions..." << "\n";
    print(a);   //0.707107+0.707107i
    print(b);   //0+2i
 
    //2. Test basic arithmetic
+   std::cout << "Test basic arithmetic..." << "\n";
    print(a*a); //0+1i
    print(a*b); //-1.41421+1.41421i
    print(a+b); //0.707107+2.707107i
    print(~a);  //0.707107+-0.707107i
+   std::cout << (a+a).mag2() << "\n";
+   std::cout << (a+a).mag() << "\n";
 
    //3. Precision test using distributive law
+   std::cout << "Precision Test using distributive law..." << "\n";
    Cmplx conjunctive((~a+a)*(b-~b));
    Cmplx disjunctive(~a*b + a*b - ~a*~b - a*~b);
    print(conjunctive); //0+5.65685i 
@@ -27,6 +32,7 @@ int main(int argc, char** argv) {
    print(conjunctive-disjunctive);//0+0i
 
    //4. Test phase-factor computations 
+   std::cout << "Test phase factor computations..." << "\n";
    print(unit(0.0));   //1+0i
    print(unit(-tau/4));//6.12323e-17+-1i
    print(unit(-tau));  //1+-0i
