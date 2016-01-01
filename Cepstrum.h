248f682173e689c43b7242ed11c704fbdae6ae16
samtenka <samtenka@umich.edu>
2016-01-01 00:50:17 -0500

    oops we'd forgotton FFT/IFFT in Cepstrum "extract essence" cepstrum averaging subroutine.

--- Cepstrum.h
+++ Cepstrum.h
@@ -2,15 +2,17 @@
 #define CEPSTRUM_H
 #include "Array.h"
 #include "Cmplx.h"
+#include "FFT.h"
 
 typedef const char* filename;
 
 void extract_essence(Array<filename> &ins, filename out) {
    Array<short> first(ins.data[0]); //TODO: sorta wasteful, just to get len...
-   Array<Cmplx> avg(first.len);
+   Array<Cmplx> avg(inf2(first.len));
    for(int i=0; i<ins.len; ++i) {
       Array<short> next(ins.data[i]);
-      Array<Cmplx> summand(next);
+      Array<Cmplx> summand(inf2(next.len));
+      FFT(next,summand);
       for(int j=0; j<avg.len; ++j) {
          summand.data[j] = log(summand.data[j]);
       }
@@ -24,8 +26,10 @@ void extract_essence(Array<filename> &ins, filename out) {
    for(int j=0; j<avg.len; ++j) {
       avg.data[j] = exp(avg.data[j]);
    }
-   Array<short> outarray(avg);
-   outarray.write_to(out);
+   Array<Cmplx> outarray(inf2(avg.len));
+   IFFT(avg,outarray);
+   Array<short> shorts(outarray);
+   shorts.write_to(out);
 }
 
 #endif//CEPSTRUM_H
--- CepstrumTest.cpp
+++ CepstrumTest.cpp
@@ -1,10 +1,10 @@
 #include "Cepstrum.h"
 
 int main(int argc, char** argv) {
-   Array<filename> ins(3);
+   Array<filename> ins(2);
    ins.data[0] = "Audio/oooh0138_00.wav";
    ins.data[1] = "Audio/oooh0138_01.wav";
-   ins.data[2] = "Audio/oooh0138_02.wav";
+   //ins.data[2] = "Audio/oooh0138_02.wav";
    filename out = "Audio/oooh0138_avgd.wav";
    extract_essence(ins, out);
    return 0;
