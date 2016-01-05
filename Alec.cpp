21c27df53c09d3d10848e0402701d1272e85660c
samtenka <samtenka@umich.edu>
2016-01-04 21:29:22 -0500

    alec

--- Alec.cpp
+++ Alec.cpp
@@ -19,7 +19,7 @@ void write_ifft(float(*myfunc)(float), float duration, char * const filename) {
 }
 
 float testfunc(float f) {
-   return 32000*440.0 / f; //sin(f * 400/sin(f));
+   return 400/sin(f);
 }
 #include <stdlib.h>
 int main(int argc, char** argv) {
