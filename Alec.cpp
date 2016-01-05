fc6af911b6b8f9ae328f25cdd9c4265785885c52
samtenka <samtenka@umich.edu>
2016-01-04 21:20:12 -0500

    alec

--- Alec.cpp
+++ Alec.cpp
@@ -1,14 +1,16 @@
 #include "Array.h"
 #include "Cmplx.h"
 #include "FFT.h"
+#include <math.h>
 
 const long int SAMPLE_RATE = 44100;
 void write_ifft(float(*myfunc)(float), float duration, char * const filename) {
    const long int samples = duration*SAMPLE_RATE;
    Array<Cmplx> ifft_me(samples); 
+   const float norm = sqrt(ifft_me.len);
    for(int i=0; i<samples; ++i) {
       float x = static_cast<float>(i)/SAMPLE_RATE;
-      ifft_me.data[i] = Cmplx(myfunc(x), 0.0);
+      ifft_me.data[i] = Cmplx(myfunc(x), 0.0) * norm;
    }
    Array<Cmplx> time_domain(ifft_me.len);
    IFFT(ifft_me, time_domain);
@@ -16,7 +18,6 @@ void write_ifft(float(*myfunc)(float), float duration, char * const filename) {
    write_me.write_to(filename);
 }
 
-#include <math.h>
 float testfunc(float f) {
    return 32000*440.0 / f; //sin(f * 400/sin(f));
 }
